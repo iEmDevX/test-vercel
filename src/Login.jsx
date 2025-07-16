3299f4925d1a08d787196eb5a2af66d95a7cfb1b
watcharapongsr <watcharapong.sr@kbtg.tech>
2025-07-16 14:00:39 +0700

    remove react router dom

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router'
 
 function Login() {
   const [email, setEmail] = useState('')
